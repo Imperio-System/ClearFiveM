97038ca420729ac47804e0434a2e94621b34390a
Alejandro Font <fontalejandro0@gmail.com>
2024-05-31 17:20:39 +0200

    Agregar archivos de proyecto.

---
+++ ClerarFivem/ClearCache.cs
@@ -0,0 +1,34 @@
+﻿namespace ClerarFivem
+{
+    internal class ClearCache
+    {
+        static public void Clear()
+        {
+            string defaultPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "FiveM\\FiveM.app");
+
+            if (Utilities.IsProcessRunning("FiveM"))
+            {
+                Console.WriteLine("FiveM is currently running. Please close the application before cleaning the cache.");
+                return;
+            }
+
+            if (Directory.Exists(defaultPath))
+            {
+                var dataPath = Path.Combine(defaultPath, "data");
+                if (Directory.Exists(dataPath))
+                {
+                    Utilities.DeleteDirectory(dataPath);
+                    Console.WriteLine("Cache cleaned successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Data directory does not exist.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The default installation path for FiveM does not exist.");
+            }
+        }
+    }
+}
---
+++ ClerarFivem/Program.cs
@@ -0,0 +1,57 @@
+﻿using ClerarFivem;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            // Limpiar la consola
+            Console.Clear();
+
+            // Información del creador y versión del programa con arte ASCII
+            Utilities.PrintAsciiArt();
+            Console.WriteLine();
+            Console.WriteLine("Welcome to ClearFiveM by Imperio Company v1.0");
+            Console.WriteLine();
+
+            // Mostrar menú de opciones
+            ShowMenu();
+
+            // Leer opción del usuario
+            string option = Console.ReadLine() ?? string.Empty;
+
+            // Limpiar la consola antes de realizar la acción
+            Console.Clear();
+
+            switch (option)
+            {
+                case "1":
+                    ClearCache.Clear();
+                    break;
+                case "2":
+                    ClearLogsAndCrashes.Clear();
+                    break;
+                case "4":
+                    Console.WriteLine("Thanks for using ClearFiveM by Imperio Company. Goodbye!");
+                    return;
+                default:
+                    Console.WriteLine("Invalid option. Please select a valid option.");
+                    break;
+            }
+
+            // Esperar que el usuario presione una tecla para volver al menú
+            Console.WriteLine("Press any key to return to the menu...");
+            Console.ReadKey();
+        }
+    }
+
+    static void ShowMenu()
+    {
+        Console.WriteLine("Please select an option:");
+        Console.WriteLine("1. Clean Cache");
+        Console.WriteLine("2. Clean Logs and Crashes");
+        Console.WriteLine("4. Exit");
+        Console.Write("Option: ");
+    }
+}
